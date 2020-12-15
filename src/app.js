7c7918ef67c9e630022c1a0d0e9200889a0285fc
Your username <suhaha.git@gmail.com>
2020-12-15 12:12:49 +0200

    completed code in app.js - recursion exes

--- src/app.js
+++ src/app.js
@@ -4,24 +4,28 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
+const findFactorial = function(num) {
   //Your code here
-
+  if(num === 0){return 1}
+  return n * findFactorial(n-1)
 }
 
 //Exercise 2
-const reverseString = function() {
+const reverseString = function(str) {
   //Your code here
-
+  if (str === '') {return ""}
+  return reverseString(str.substr(1)) + str.charAt(0)
 }
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
-
-const swap = function() {
+const swap = function(arr1, arr2) {
   //Your code here
-
+  if(arr1.length === 0){
+    return 
+  }
+  arr2.push(arr1[0])
+  arr1.shift()
+  return swap(arr1, arr2)
 }
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
